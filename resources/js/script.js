5d8f03417ce03d8c215270ce4cca4fd1bc091861
luis miguel tolentino <luis miguel tolentino>
2016-06-24 15:31:33 -0400

    Agregar la barra sticky

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-    
+    $('.js-section1').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, { 
+        offset: '80px;'
+    });
 });
